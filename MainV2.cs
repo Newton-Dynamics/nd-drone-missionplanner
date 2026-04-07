cc36804d0d7d7ef80dab0a8256aefc9441477614
unknown <hussein.sleiman@newton-dynamics.com>
2026-04-07 20:02:45 +0200

    added global reboot shortcut for autopilot. Key plus R

--- MainV2.cs
+++ MainV2.cs
@@ -4116,6 +4116,28 @@ namespace MissionPlanner
                 return true;
             }
 
+
+            if (keyData == (Keys.Control | Keys.R)) // reboot autopilot
+            {
+                if (comPort.BaseStream.IsOpen)
+                {
+                    if (CustomMessageBox.Show("Are you sure you want to reboot the autopilot?", "Reboot",
+                            MessageBoxButtons.YesNo) == (int)DialogResult.Yes)
+                    {
+                        try
+                        {
+                            comPort.doReboot(false, true);
+                        }
+                        catch
+                        {
+                            CustomMessageBox.Show(Strings.CommandFailed, Strings.ERROR);
+                        }
+                    }
+                }
+
+                return true;
+            }
+
             if (keyData == (Keys.Control | Keys.W)) // test ac config
             {
                 new PropagationSettings().Show();
